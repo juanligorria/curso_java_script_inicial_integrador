d8ab371d1b8d61bb656c9b08554534b926c7cdc2
juanligorria <juanlig63@gmai.com>
2023-09-14 11:56:29 -0300

    ejercicio promedios de notas

--- js/practica.js
+++ js/practica.js
@@ -5,3 +5,21 @@ const estudiantes = [
     { nombre: "Pedro", edad: 21, notas: [80, 82, 88, 90, 85] },
     { nombre: "Daniel", edad: 21, notas: [85, 80, 61, 90, 82] }
 ]
+
+let acumulador = 0;
+let contador = 0;
+let promedio = 0;
+
+for (let index = 0; index < estudiantes.length; index++) {
+    const estudiante = estudiantes [index];
+    const notas = estudiante.notas;
+    document.write(notas);
+
+    for (let j = 0; j < notas.length; j++) {
+        const nota = notas[j];
+        acumulador = acumulador + nota;
+        contador = contador +1;  
+    }
+promedio = acumulador/contador;
+}
+console.log(promedio)
