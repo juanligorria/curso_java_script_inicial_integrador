147fb1a07b6b1a91bffe6535a30362e80077472c
juanligorria <juanlig63@gmai.com>
2023-09-14 10:36:04 -0300

    trabajo 1

---
+++ js/practica.js
@@ -0,0 +1,7 @@
+const estudiantes = [
+    { nombre: "Juan", edad: 20, notas: [85, 98, 78, 88, 90] },
+    { nombre: "María", edad: 21, notas: [75, 80, 92, 89, 78] },
+    { nombre: "Carlos", edad: 22, notas: [95, 88, 76, 92, 84] },
+    { nombre: "Pedro", edad: 21, notas: [80, 82, 88, 90, 85] },
+    { nombre: "Daniel", edad: 21, notas: [85, 80, 61, 90, 82] }
+]
