a7f2ee7f51af259226747fe4e8c09165e07eab14
juanligorria <juanlig63@gmai.com>
2023-10-01 01:10:18 -0300

    practica

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # curso_java_script_inicial_integrador
 Tareas del curso de java script de utn.
 Juan Ligorria
+{
+    "estudiantes": [
+        {
+            "nombre": "Juan",
+            "promedio": 7
+        },
+        {
+            "nombre": "Pedro",
+            "promedio": 9
+        },
+        {
+            "nombre": "Maria",
+            "promedio": 8
+        },
+        {
+            "nombre": "Carlos",
+            "promedio": 10
+        },
+        {
+            "nombre": "Ana",
+            "promedio": 7
+        },
+        {
+            "nombre": "Pablo",
+            "promedio": 10
+        },
+        {
+            "nombre": "Matias",
+            "promedio": 6
+        },
+        {
+            "nombre": "Ezequiel",
+            "promedio": 8
+        },
+        {
+            "nombre": "Florencia",
+            "promedio": 9
+        },
+        {
+            "nombre": "Roxana",
+            "promedio": 7
+        }
+    ]
+}
--- js/trabajo2.js
+++ js/trabajo2.js
@@ -1,2 +1,87 @@
-let prueba= document.querySelector(".titulito");
-document.write(prueba)
+// let box = document.getElementById("caja_notas");
+
+// box.addEventListener("mouseover", e => {box.style.backgroundColor = "grey"})
+
+
+
+
+
+
+const cursoJSON = `{
+    "estudiantes": [
+        {
+            "nombre": "Juan",
+            "promedio": 7
+        },
+        {
+            "nombre": "Pedro",
+            "promedio": 9
+        },
+        {
+            "nombre": "Maria",
+            "promedio": 8
+        },
+        {
+            "nombre": "Carlos",
+            "promedio": 10
+        },
+        {
+            "nombre": "Ana",
+            "promedio": 7
+        },
+        {
+            "nombre": "Pablo",
+            "promedio": 10
+        },
+        {
+            "nombre": "Matias",
+            "promedio": 6
+        },
+        {
+            "nombre": "Ezequiel",
+            "promedio": 8
+        },
+        {
+            "nombre": "Florencia",
+            "promedio": 9
+        },
+        {
+            "nombre": "Roxana",
+            "promedio": 7
+        }
+    ]
+}`
+
+const curso = JSON.parse(cursoJSON);
+let arrayTp = curso.estudiantes
+
+let box = document.getElementById("caja_notas")
+
+let contador=0
+let acumulador=0
+let promediogral=0
+
+arrayTp.forEach(estudiantes => {
+   
+    const lista = "<p>"+estudiantes.nombre + ", promedio: " + estudiantes.promedio +"</p>";
+
+  box.insertAdjacentHTML("afterbegin", lista);
+
+})
+
+// promedio
+
+for (let index = 0; index < arrayTp.length; index++) {
+    const estudiante = curso.estudiantes [index];
+    const promedio = estudiante.promedio;
+    
+        acumulador = acumulador + promedio;
+        contador = contador +1;  
+    }
+promediogral= acumulador/contador;
+
+document.write(`El promedio general es: `, promediogral);
+
+// Alumnos con 10
+const promedio10 = estudiantes.promedio;
+console.log(promedio10)
