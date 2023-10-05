3209c3d04f8206f6fa5104ad3eb5b1bac9de3866
juanligorria <juanlig63@gmai.com>
2023-10-05 12:25:10 -0300

    adelanto tp3

--- js/trabajo3.js
+++ js/trabajo3.js
@@ -1,8 +1,13 @@
-// document.querySelector("input").value
-const modalOn = document.querySelector(".modalOn")
-const modal = document.getElementById("modal")
-
-const modalOff = document.querySelector(".cerrarmodal")
+const modalOn = document.querySelector(".modalOn");
+const modal = document.getElementById("modal");
+const cajaInterior = document.getElementById("cajainterior")
+const cajacierre = document.getElementsByClassName("cerrarmodal");
+const input = document.getElementById("inputmodal");
+const modalOff = document.querySelector(".cerrarmodal");
+const botonEnviar = document.getElementById("enviar");
 
 modalOn.addEventListener("click", () => {modal.style.display = `flex`});
 modalOff.addEventListener("click", () => {modal.style.display = `none`});
+// input.addEventListener("keydown",  function(event)
+//  {if (event.key === "Enter") modal.style.display = `none`})
+botonEnviar.addEventListener("click", () => {modal.style.display = `none`})
