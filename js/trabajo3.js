8e189de940088ca2dd0fd4f89fccc2fdfa81d2f9
juanligorria <juanlig63@gmai.com>
2023-10-06 10:41:49 -0300

    finalizo tp3

--- js/trabajo3.js
+++ js/trabajo3.js
@@ -7,7 +7,8 @@ const modalOff = document.querySelector(".cerrarmodal");
 const botonEnviar = document.getElementById("enviar");
 
 modalOn.addEventListener("click", () => {modal.style.display = `flex`});
+
 modalOff.addEventListener("click", () => {modal.style.display = `none`});
-// input.addEventListener("keydown",  function(event)
-//  {if (event.key === "Enter") modal.style.display = `none`})
-botonEnviar.addEventListener("click", () => {modal.style.display = `none`})
+
+botonEnviar.addEventListener("click", () => {modal.style.display = `none`, 
+console.log(input.value)})
