69697032bc08dae02b8ff5847ddaa2953cc43865
juanligorria <juanlig63@gmai.com>
2023-10-11 06:37:29 -0300

    11 de octubre

--- js/practica.js
+++ js/practica.js
@@ -1,12 +1,51 @@
-// let boton = document.querySelector(".btn")
-// boton.addEventListener("click", function (event)
-// {console.log("marchok", event.button)
-// if (event.button == 0) {console.log("izquierda")}
-// });
-// document.addEventListener("click", function (event) 
-// {console.log("paposo", event.target.nodename)})
+// let pedido = new XMLHttpRequest();
+// pedido.open('GET', '../Json/estudiantes.json', true,);
 
-let botones= document.querySelectorAll(".btn");
-botones.forEach(boton => boton.addEventListener
-    ("click", function(event) {console.log("muy dificil") }))
-.style {color }
+
+
+
+// pedido.onreadystatechange = ()=> {console.log(pedido)
+
+// if (pedido.readyState == 4) {console.log("ready")}
+
+
+// estudiantesTxt = pedido.response
+// estudiantes = JSON.parse(estudiantesTxt)
+// console.log(estudiantes)
+// }
+// pedido.send();
+
+
+
+// pedir a una api y obtener un JSON+boton que muestra el resultado
+
+let boton = document.querySelector("button");
+boton.addEventListener("click", (event) => {
+    let peticion = new XMLHttpRequest();
+
+peticion.open('GET', "https://api.thecatapi.com/v1/images/search", true); 
+
+peticion.onreadystatechange = () => {
+
+    if (peticion.readyState == 4 && peticion.status==200) {
+        resultado=JSON.parse(peticion.responseText);
+        let gatuno = resultado[0];
+        // console.log(gatuno);
+
+        let urlGato = gatuno.url
+
+        let imagen = document.querySelector ("img")
+        imagen.src=urlGato
+        console.log(urlGato);
+}
+}
+peticion.send();
+
+})
+localStorage.setItem = ("nombre", "juan")
+
+
+
+
+
+
